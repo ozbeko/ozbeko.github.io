e849eb130251296bb8cfa71d3d94ac5b9fb02d96
Ömer Faruk Özbek <omer.ozbek.faruk@gmail.com>
2022-02-06 11:56:32 +0300

    index.js is modified

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
 $(".top-cloud").animate({"right": "20%"}, 2000);
 $(".bottom-cloud").animate({"left": "20%"}, 2000);
 $(".mountain").animate({"opacity": "1"}, 4000);
-
-$(".linkedin").click(function(){
-  location.href = "https://tr.linkedin.com/in/oezbek-oemer";
-});
-
-$(".fa-github").click(function(){
-  location.href = "https://github.com/ozbeko";
-});
-
-$(".fa-envelope").click(function(){
-  location.href = "mailto:omer.ozbek.faruk@email.com";
-});
